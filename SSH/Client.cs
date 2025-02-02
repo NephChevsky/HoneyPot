b41a1b602555414d6fdfce9814dfca47cdd29338
Neph <philippe.balleydier@gmail.com>
2025-02-02 13:36:07 +0100

    don't redo protocol exchange every time

--- SSH/Client.cs
+++ SSH/Client.cs
@@ -1,6 +1,5 @@
 ﻿using Serilog;
 using System.Net.Sockets;
-using System.Reflection.PortableExecutable;
 using System.Text;
 
 namespace HoneyPot.SSH
@@ -8,6 +7,11 @@ namespace HoneyPot.SSH
 	internal class Client
 	{
 		private Socket _socket;
+		private NetworkStream _networkStream;
+		private StreamWriter _streamWriter;
+		private StreamReader _streamReader;
+
+		private bool _protocolVersionExchangeCompleted;
 
 		public bool IsConnected
 		{
@@ -20,6 +24,10 @@ namespace HoneyPot.SSH
 		public Client(Socket socket)
 		{
 			_socket = socket;
+
+			_networkStream = new(_socket);
+			_streamWriter = new(_networkStream, Encoding.ASCII);
+			_streamReader = new(_networkStream, Encoding.ASCII);
 		}
 
 		public void Poll()
@@ -29,23 +37,18 @@ namespace HoneyPot.SSH
 				return;
 			}
 
-			NetworkStream stream = new(_socket);
-			StreamReader reader = new(stream, Encoding.ASCII);
-			StreamWriter writer = new(stream, Encoding.ASCII);
-
-			// Step 1: Send SSH protocol version string to the client
-			if (reader.Peek() == 0) // Check if we haven't already sent a version string
+			if (!_protocolVersionExchangeCompleted)
 			{
+				// Step 1: Send SSH protocol version string to the client
 				string versionString = "SSH-2.0-Open SSH for _Windows_ 9.5\r\n"; // Server's version string
-				writer.Write(versionString);
-				writer.Flush();
-			}
+				_streamWriter.Write(versionString);
+				_streamWriter.Flush();
 
-			// Step 2: Wait for the client's response to the protocol version
-			if (reader.Peek() > 0)
-			{
-				string clientVersion = reader.ReadLine();
+				// Step 2: Wait for the client's response to the protocol version
+				string clientVersion = _streamReader.ReadLine();
+				_protocolVersionExchangeCompleted = true;
 				Log.Information("Received client version: {ClientVersion}", clientVersion);
+				return;
 			}
 		}
 	}
